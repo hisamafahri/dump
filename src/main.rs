a80478b83589535de96426a986de43488fe0b183
Hisam Fahri <me@hisamafahri.com>
2022-04-26 14:19:13 +0700

    feat(asd): asd

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use futures_lite::future;
+use std::process;
+
+fn main() {
+    return future::block_on(async {
+        let cmd = process::Command::new("git")
+            .args(["push", "origin", "master"])
+            .stderr(std::process::Stdio::piped())
+            .stdout(std::process::Stdio::piped())
+            .stdin(std::process::Stdio::null())
+            .output()
+            .expect("\x1B[41;1m\x1B[37;1m ERROR: \x1B[0m\x1B[0m failed to run command");
+
+        println!("{}", String::from_utf8_lossy(&cmd.stderr));
+        println!("{}", String::from_utf8_lossy(&cmd.stdout));
+    });
+}
